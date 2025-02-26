3cdeb7bfe3091dbe85ecf44a2caac75b82e3da86
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-26 18:14:35 -0500

    feat:TodoController method listAll

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -1,16 +1,31 @@
 package com.todo.controller;
 
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.todo.controller.dtro.TodoDto;
+import com.todo.service.TodoService;
+
+import lombok.RequiredArgsConstructor;
 
 
 
 @RestController
 @RequestMapping("/todos")
+@RequiredArgsConstructor
 public class TodoController {
 
-    
+    private final TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<List<TodoDto>> listAll() {
+        return ResponseEntity.ok(todoService.listAll());
+    }
    
 
 }
