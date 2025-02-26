e273eae69b06ba33549d49e62f80e8cd98ea0b5c
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-26 17:53:32 -0500

    feat: TodoService - ListAll

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -1,26 +1,16 @@
 package com.todo.controller;
 
-import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.todo.controller.dtro.TodoDto;
+
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
 
-    @GetMapping("/{id}")
-    public ResponseEntity<TodoDto> ListById(@PathVariable("id") UUID id){
-        //TodoDto dto = todoService.listById(id);
-        //Return ResponseEntity.ok(dto);
-        return null;
-    }
-
+    
    
 
 }
--- src/main/java/com/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/todo/service/impl/TodoServiceImpl.java
@@ -1,10 +1,26 @@
 package com.todo.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.todo.controller.dtro.TodoDto;
+import com.todo.repository.TodoRepository;
 import com.todo.service.TodoService;
+import com.todo.service.mapper.TodoMapper;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService {
     
+    private final TodoRepository todoRepository;
+
+    @Override
+    public List<TodoDto> listAll() {
+        return todoRepository.findAll()
+            .stream()
+            .map(e -> TodoMapper.ToDto(e)).toList();
+    }
 }
