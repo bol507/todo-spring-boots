9a5552f89ff619efaa599bc04d98321ba42c6c51
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-26 17:29:47 -0500

    feat: todo entity

---
+++ src/main/java/com/todo/repository/entity/TodoEntity.java
@@ -0,0 +1,39 @@
+package com.todo.repository.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "todos")
+@Getter
+@Setter
+public class TodoEntity extends AbstractEntity {
+
+    @Column(name = "task", nullable = false)
+    private String task;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "date_created")
+    private LocalDate dateCreated;
+
+    @Column(name = "is_completed")
+    private boolean isCompleted;
+
+    @Column(name = "is_archived")
+    private boolean isArchived;
+
+    @Column(name = "is_reminder")
+    private boolean isReminder;
+
+}
